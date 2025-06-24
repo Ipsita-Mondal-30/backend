aeee61ca346ba8d71adf70f109e243a07f798258
Ipsita <ipsitaamondal@gmail.com>
2025-06-24 23:07:24 +0530

    add subscription model for user channel subscriptions

---
+++ src/models/subscription.models.js
@@ -0,0 +1,15 @@
+import mongoose ,{Schema } from "mongoose";
+
+const SubscriptionSchema=new Schema({
+    subscriber:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    channel:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    }
+},{timestamps:true})
+export const SubscriptionModel=mongoose.model("Subscription",SubscriptionSchema)
